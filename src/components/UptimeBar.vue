caa6292f983d1c7e16a89c2c3f850874b6ebb6c2
caraka15 <carakawidi07@gmail.com>
2025-10-14 02:10:20 +0700

    toggle switch

--- src/components/UptimeBar.vue
+++ src/components/UptimeBar.vue
@@ -10,15 +10,15 @@ const secondRow = computed(() => props.blocks?.slice(25, 50));
 
 </script>
 <template>
-  <div class="inline-block bg-base-200 p-1 rounded-lg border border-gray-200 dark:border-gray-700">
-    <div class="flex flex-wrap gap-0.5 items-center mb-0.5">
+  <div class="bg-base-200 p-1 rounded-lg border border-gray-200 dark:border-gray-700">
+    <div class="flex flex-wrap justify-between items-center mb-0.5">
       <div class="cursor-default" v-for="(item, index) in firstRow" :key="index">
         <div class="tooltip" :data-tip="item.height">
           <div style="width: 10px; height: 10px;" class="rounded-sm" :class="item.color">&nbsp;</div>
         </div>
       </div>
     </div>
-    <div class="flex flex-wrap gap-0.5 items-center">
+    <div class="flex flex-wrap justify-between items-center">
       <div class="cursor-default" v-for="(item, index) in secondRow" :key="index">
         <div class="tooltip" :data-tip="item.height">
           <div style="width: 10px; height: 10px;" class="rounded-sm" :class="item.color">&nbsp;</div>
