1a510bf5a4d4bf2ac016e96934eaaca3a81c1f72
caraka15 <carakawidi07@gmail.com>
2025-10-14 00:46:48 +0700

    toggle switch

--- src/components/UptimeBar.vue
+++ src/components/UptimeBar.vue
@@ -1,15 +1,28 @@
 <script lang="ts" setup>
-import type { PropType } from 'vue';
+import { computed, type PropType } from 'vue';
 
 const props = defineProps({
   blocks: { type: Array as PropType<{ height: string; color: string }[]> },
 });
+
+const firstRow = computed(() => props.blocks?.slice(0, 50));
+const secondRow = computed(() => props.blocks?.slice(50, 100));
+
 </script>
 <template>
-  <div class="flex gap-1 items-center">
-    <div class="cursor-default" v-for="(item, index) in blocks" :key="index">
-      <div class="tooltip" :data-tip="item.height">
-        <div class="w-2 h-2 rounded-full" :class="item.color">&nbsp;</div>
+  <div class="bg-base-200 p-2 rounded-lg border border-gray-200 dark:border-gray-700">
+    <div class="flex gap-1 items-center mb-1">
+      <div class="cursor-default" v-for="(item, index) in firstRow" :key="index">
+        <div class="tooltip" :data-tip="item.height">
+          <div class="w-2 h-2 rounded-full" :class="item.color">&nbsp;</div>
+        </div>
+      </div>
+    </div>
+    <div class="flex gap-1 items-center">
+      <div class="cursor-default" v-for="(item, index) in secondRow" :key="index">
+        <div class="tooltip" :data-tip="item.height">
+          <div class="w-2 h-2 rounded-full" :class="item.color">&nbsp;</div>
+        </div>
       </div>
     </div>
   </div>
