886623131f4e4e9550d389808a17e845bd89ce06
caraka15 <carakawidi07@gmail.com>
2025-10-13 23:20:15 +0700

    toggle switch

--- src/components/UptimeBar.vue
+++ src/components/UptimeBar.vue
@@ -6,9 +6,11 @@ const props = defineProps({
 });
 </script>
 <template>
-  <div class="flex gap-0.5">
+  <div class="flex gap-1 items-center">
     <div class="cursor-default" v-for="(item, index) in blocks" :key="index">
-      <div class="tooltip" :data-tip="item.height" :class="item.color" style="width: 3px">&nbsp;</div>
+      <div class="tooltip" :data-tip="item.height">
+        <div class="w-2 h-2 rounded-full" :class="item.color">&nbsp;</div>
+      </div>
     </div>
   </div>
 </template>
