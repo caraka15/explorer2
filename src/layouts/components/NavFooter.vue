ecf94039d3b5aa195e5ed8741a287765eb86460f
caraka15 <carakawidi07@gmail.com>
2025-10-17 15:06:06 +0700

    add uptime to validator page

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -3,10 +3,9 @@
   <footer class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full">
     <div class="flex flex-1">
       &copy;&nbsp;
-      {{ new Date().getFullYear() }}&nbsp; Made With&nbsp; <img src="../../assets/images/heart.svg" />&nbsp; By&nbsp;
-      <a class="link link-primary no-underline" href="https://crxanode.me" target="_blank" rel="noopener noreferrer"
-        >Crxanode</a
-      >
+      {{ new Date().getFullYear() }}&nbsp; made by&nbsp;
+      <a class="link link-primary no-underline" href="https://ping.pub" target="_blank" rel="noopener noreferrer">ping.pub</a>, modified by&nbsp;
+      <a class="link link-primary no-underline" href="https://crxanode.me" target="_blank" rel="noopener noreferrer">crxanode</a>
     </div>
     <div class="hidden md:!block">
       <a
